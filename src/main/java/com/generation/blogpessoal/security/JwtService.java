ba2bec4217ab15c741e1c5330903d7d4dc074f5f
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2024-06-28 12:07:04 -0300

    ajustado security

--- src/main/java/com/generation/blogpessoal/security/JwtService.java
+++ src/main/java/com/generation/blogpessoal/security/JwtService.java
@@ -57,6 +57,7 @@ public class JwtService {
 					.setClaims(claims)
 					.setSubject(userName)
 					.setIssuedAt(new Date(System.currentTimeMillis()))
+					.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
 					.signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
 	}
 
